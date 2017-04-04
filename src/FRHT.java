302b06cdd58d8adcbbd2e344b6a880170ec1cfc9
RIZ-PC\haqiqi <rzkhqq@gmail.com>
2017-04-04 17:54:14 +0700

    add randomize, and put edge on a window into candidate

--- src/FRHT.java
+++ src/FRHT.java
@@ -6,10 +6,15 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 import java.util.logging.Logger;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Rect;
 import org.opencv.core.CvType;
 import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.*;
@@ -36,17 +41,78 @@ public class FRHT {
 	     out.close(); 
 	 }
 	 
+	
+	 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Mat grayscaleMat = Imgcodecs.imread("test.jpg", Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);		
+		int w=40; 
+		
+		class Pair<T>{
+			 T x;
+			 T y;
+			 Pair(T x, T y){this.x=x;this.y=y;}
+		 }
+		
+		class Cans<T>{
+			 T x;
+			 T y;
+			 T z;
+			 Cans(T x, T y, T z){this.x=x;this.y=y;this.z=z;}
+		 }
+
+		Mat grayscaleMat = Imgcodecs.imread("data/test.jpg", Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);		
 		
 	
         //convert to binary
         Mat binaryMat = new Mat(grayscaleMat.size(),grayscaleMat.type());
-        Imgproc.threshold(grayscaleMat, binaryMat, 1, 255, Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);
-        Mat binary = binaryMat.clone();
+        Imgproc.threshold(grayscaleMat, binaryMat, 0, 255, Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);
+        ArrayList<Pair<Integer>> edge = new ArrayList<Pair<Integer>>(); 
+        for(int i =0+w; i<binaryMat.rows()-w;i++){
+        	for(int j=0+w;j<binaryMat.cols()-w;j++){
+        		if(binaryMat.get(i,j)[0]==0){
+        			edge.add(new Pair<Integer>(i,j));
+        			//System.out.println(String.valueOf(i)+" "+String.valueOf(j)+"\n");
+        		}
+        	}
+        }
         
-        dumper(binary, "ehe1.csv");
+        Random rand = new Random();
+        
+        int pos = rand.nextInt(edge.size()-0+1)+0;
+        System.out.println(pos);
+        Pair<Integer> selected = edge.get(pos);
+        System.out.println(String.valueOf(selected.x)+" "+String.valueOf(selected.y));
+        //Rect rect = new Rect(selected.x-w , selected.y-w );
+        
+        Rect rect = new Rect(selected.x-w, selected.y-w,2*w,2*w);
+        Mat roi = new Mat(binaryMat, rect);
+        
+        Map<Double, Pair<Integer>> counter = new HashMap<Double, Pair<Integer>>();
+        
+        
+        ArrayList<Cans<Pair<Integer>>> cans = new ArrayList<Cans<Pair<Integer>>>();
+        for(int i =0; i<roi.rows();i++){
+        	for(int j=0;j<roi.cols();j++){
+        		if(roi.get(i,j)[0]==0){
+        			Double d = Math.sqrt((selected.y-i)^2+(selected.x-j)^2);
+        			if(counter.get(d)==null){
+        				counter.put(d, new Pair<Integer>(i,j));
+        			}else{
+        				cans.add(new Cans<Pair<Integer>>(selected, counter.get(d), new Pair<Integer>(i,j)));
+        				counter.remove(d);
+        				System.out.println("out here");
+        			}
+        			//edge.add(new Pair<Integer>(i,j));
+        			//System.out.println(String.valueOf(i)+" "+String.valueOf(j)+"\n");
+        		}
+        	}
+        }
+        
+        
+        //Mat binary = binaryMat.clone();
+        
+        
+        dumper(binaryMat, "ehe1.csv");
 	}
 
 }
