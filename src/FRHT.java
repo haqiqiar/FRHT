d65d9b4c21cea91b78b538d7e1cea03fdb7d2b5f
RIZ-PC\haqiqi <rzkhqq@gmail.com>
2017-05-08 20:20:54 +0700

    candidate using three edge points

--- src/FRHT.java
+++ src/FRHT.java
@@ -7,6 +7,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -14,6 +16,8 @@ import java.util.logging.Logger;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfInt;
 import org.opencv.core.Rect;
 import org.opencv.core.CvType;
 import org.opencv.core.Scalar;
@@ -22,102 +26,174 @@ import org.opencv.imgproc.Imgproc;
 
 public class FRHT {
 	static{System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
-	
-	 public static void printArr( byte[][] arr )
-	    {
-	        for ( int y=0; y<arr.length; ++y )
-	        {
-	            for ( int x=0; x<arr[y].length; ++x )
-	                System.out.print( arr[y][x] + ", " ); // Was arr[x][y] in prev code.
-	            System.out.println("");
-	        }
 
-	    }
-	 public static void dumper(Mat m, String name) throws IOException{
-		 File f = new File(name);
-		 f.createNewFile();
-		 PrintWriter out = new PrintWriter(f, "UTF-8");
-	     out.println(m.dump());
-	     out.close(); 
-	 }
-	 
+	public static void printArr( byte[][] arr )
+	{
+		for ( int y=0; y<arr.length; ++y )
+		{
+			for ( int x=0; x<arr[y].length; ++x )
+				System.out.print( arr[y][x] + ", " ); // Was arr[x][y] in prev code.
+			System.out.println("");
+		}
+
+	}
+	public static void dumper(Mat m, String name) throws IOException{
+		File f = new File(name);
+		f.createNewFile();
+		PrintWriter out = new PrintWriter(f, "UTF-8");
+		out.println(m.dump());
+		out.close(); 
+	}
 	
-	 
+	
+
+
+
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		int w=90; 
-		
-		class Pair<T>{
-			 T x;
-			 T y;
-			 Pair(T x, T y){this.x=x;this.y=y;}
-		 }
-		
-		class Cans<T>{
-			 T x;
-			 T y;
-			 T z;
-			 Cans(T x, T y, T z){this.x=x;this.y=y;this.z=z;}
-		 }
+		int w=60; 
+
+		class Point{
+			Integer x;
+			Integer y;
+			Point(Integer x, Integer y){this.x=x;this.y=y;}
+		}
+
+		class Pair{
+			Point a;
+			Point b;
+			Pair(Point a, Point b){this.a=a;this.b=b;}
+		}
+
+		class Cans{
+			Point seed;
+			ArrayList<Pair> points;
+			Integer acc;
+			Cans(Point seed){this.seed=seed;points = new ArrayList<Pair>();}
+		}
 
 		Mat grayscaleMat = Imgcodecs.imread("data/test.jpg", Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);		
+
+
+		//convert to binary
+		Mat binaryMat = new Mat(grayscaleMat.size(),grayscaleMat.type());
+		//dumper(binaryMat, "ehe3.csv");
+		Imgproc.threshold(grayscaleMat, binaryMat, 0, 255, Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);
+
+		//extract points
+		ArrayList<Point> edge = new ArrayList<Point>(); 
+		for(int i =0+w; i<binaryMat.rows()-w;i++){
+			for(int j=0+w;j<binaryMat.cols()-w;j++){
+				if(binaryMat.get(i,j)[0]==0){
+					edge.add(new Point(j,i));
+				}
+			}
+		}
+
+		ArrayList<Cans> cans = new ArrayList<Cans>();
+		//System.out.println(binaryMat.size());
+		//System.out.println(binaryMat.get(5,1)[0]);
 		
-	
-        //convert to binary
-        Mat binaryMat = new Mat(grayscaleMat.size(),grayscaleMat.type());
-        Imgproc.threshold(grayscaleMat, binaryMat, 0, 255, Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);
-        ArrayList<Pair<Integer>> edge = new ArrayList<Pair<Integer>>(); 
-        for(int i =0+w; i<binaryMat.rows()-w;i++){
-        	for(int j=0+w;j<binaryMat.cols()-w;j++){
-        		if(binaryMat.get(i,j)[0]==0){
-        			edge.add(new Pair<Integer>(j,i));
-        			
-        		}
-        	}
-        }
-        
-        Random rand = new Random();
-        
-        int pos = rand.nextInt(edge.size()-0+1)+0;
-        System.out.println(pos);
-        Pair<Integer> selected = edge.get(pos);
-        System.out.println(String.valueOf(selected.x)+" "+String.valueOf(selected.y));
-        
-        
-        Rect rect = new Rect(selected.x-w, selected.y-w,2*w,2*w);
-        Mat roi = new Mat(binaryMat, rect);
-        
-        Map<Double, Pair<Integer>> counter = new HashMap<Double, Pair<Integer>>();
-        
-        
-        ArrayList<Cans<Pair<Integer>>> cans = new ArrayList<Cans<Pair<Integer>>>();
-        for(int i =0; i<roi.rows();i++){
-        	for(int j=0;j<roi.cols();j++){
-        		if(roi.get(i,j)[0]==0){
-        			Double d = Math.sqrt(Math.pow((selected.y-i),2)+Math.pow((selected.x-j),2));
-        			if(counter.get(d)==null){
-        				counter.put(d, new Pair<Integer>(j,i));
-        			}else{
-        
-        				cans.add(new Cans<Pair<Integer>>(selected, counter.get(d), new Pair<Integer>(j,i)));
-        				counter.remove(d);
-        
-        			}
-        
-        		}
-        	}
-        }
-        
-       System.out.println("Candidate :");
-       for(Cans<Pair<Integer>> can : cans){
-    	   System.out.println("("+can.x.x.toString()+","+can.x.y.toString()+";"
-    			   		+can.y.x.toString()+","+can.y.y.toString()+";"
-    			   		+can.z.x.toString()+","+can.z.y.toString()+")"
-    			   		+String.valueOf(Math.sqrt(Math.pow(can.x.x-can.y.x,2)+Math.pow(can.x.y-can.y.y,2)))+"|"
-    			   		+String.valueOf(Math.sqrt(Math.pow(can.x.x-can.z.x,2)+Math.pow(can.x.y-can.z.y,2))));
-       }
-        
-        dumper(binaryMat, "ehe1.csv");
+		for(int n = 0; n<400;n++){
+			//picking random seed
+
+			Random rand = new Random();
+			int pos = rand.nextInt((edge.size()-1)-0+1)+0;
+			//System.out.println(pos);
+			Point selected = edge.get(pos);
+			//Point selected = new Point(5,1);
+//			//System.out.println(String.valueOf(selected.x)+" "+String.valueOf(selected.y));
+
+			//windowing
+			//Rect rect = new Rect(selected.x-w, selected.y-w,2*w,2*w);
+			//Mat roi = new Mat(binaryMat, rect);
+
+			//get candidate
+			Map<Double, Point> counter = new HashMap<Double, Point>();
+			
+			
+			Cans ncans = new Cans(selected);
+			for(int i =selected.y-w; i<(selected.y-w)+2*w;i++){
+				for(int j=selected.x-w;j<(selected.x-w)+2*w;j++){
+					if(binaryMat.get(i,j)[0]==0){
+						Double d = Math.sqrt(Math.pow((selected.y-i),2)+Math.pow((selected.x-j),2));
+						if(counter.get(d)==null){
+							counter.put(d, new Point(j,i));
+						}else{
+							ncans.points.add(new Pair(counter.get(d), new Point(j,i)));
+							counter.remove(d);
+						}
+					}
+				}
+			}
+			
+			
+			//dumper(roi, "ehe2.csv");
+			/*
+			if(ncans.points.size()>=106){
+				
+				dumper(roi, "ehe2.csv");
+			}*/
+			cans.add(ncans);
+		}
+
+		//System.out.println(cans.size());
+		
+		/*Sorting*/
+		Collections.sort(cans, new Comparator<Cans>(){
+
+			@Override
+			public int compare(Cans arg0, Cans arg1) {
+				// TODO Auto-generated method stub
+				if(arg0.points.size()>arg1.points.size()){
+					return -1;
+				}
+				
+				else if (arg0.points.size()<arg1.points.size()){
+					return 1;
+				}
+				return 0;
+			}
+			
+		});
+		
+		Cans candidate = cans.get(0);
+		System.out.println("Candidate: "+"("+candidate.points.size()+")"+candidate.seed.x.toString()+","+candidate.seed.y.toString());
+		
+		
+		Mat res = Mat.zeros(binaryMat.size(), CvType.CV_32FC1);
+		Integer i=0;
+		for (Pair a : candidate.points){
+			res.put(a.a.y, a.a.x, Integer.valueOf(i.toString()+"25"));
+			res.put(a.b.y, a.b.x, Integer.valueOf(i.toString()+"25"));
+			System.out.println(a.a.x.toString()+","+a.a.y.toString()+";"+a.b.x.toString()+","+a.b.y.toString());
+			i++;
+		}
+		
+		res.put(candidate.seed.y,candidate.seed.x, 128);
+		/*for(Cans can : cans){
+			System.out.println("Candidate :");
+			System.out.println(can.points.size());
+			
+			/*System.out.println("("+can.seed.x.toString()+","+can.seed.y.toString()+";"+")");
+			for(Pair now : can.points){
+				System.out.println(now.a.x.toString()+","+now.a.y.toString()+";"+now.b.x.toString()+","+now.b.y.toString());
+			}(=
+			
+			
+			}*/
+		
+		MatOfInt matInt=new MatOfInt();
+		
+		matInt.fromArray(Imgcodecs.CV_IMWRITE_PNG_COMPRESSION, 1);
+		
+		
+		
+		Imgcodecs.imwrite("lala.jpg", res, matInt);
+		
+
+		dumper(res, "ehe1.csv");
 	}
+	
 
 }
